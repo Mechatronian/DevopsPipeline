b78e4a83f6e85c285c8555b64c7bb601eef3f5ba
mechatronian <mechatron2008@gmail.com>
2020-07-05 01:04:16 +0530

    added selenium code for internet login

--- src/test/java/InternetLogin.java
+++ src/test/java/InternetLogin.java
@@ -1,8 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
+import static java.lang.Thread.*;
+
 public class InternetLogin {
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void Initialize(){
+        System.setProperty("webdriver.chrome.driver","D:\\Selenium_Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
     @Test
-    public void MainTest(){
-        System.out.println("Devops pipeline project!!");
+    public void MainTest() throws InterruptedException {
+        //Login
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+        Thread.sleep(5000);
+        String val_login = driver.findElement(By.xpath("//h2")).getText();
+        //validation
+        Assert.assertEquals(val_login.trim(),"Secure Area");
+    }
+
+    @AfterTest
+    public void Terminate(){
+        driver.quit();
     }
 }
