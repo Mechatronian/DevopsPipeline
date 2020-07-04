24c37a0b2cf00cadf32631cb9b6120955cceb5f3
mechatronian <mechatron2008@gmail.com>
2020-07-04 20:40:13 +0530

    update print output

--- src/test/java/InternetLogin.java
+++ src/test/java/InternetLogin.java
@@ -3,6 +3,6 @@ import org.testng.annotations.Test;
 public class InternetLogin {
     @Test
     public void MainTest(){
-        System.out.println("Helloworld Devops");
+        System.out.println("Devops pipeline project!!");
     }
 }
